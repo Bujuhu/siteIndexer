f2883879d17ea562b2eaaf5ab6430b8e47d9bb86
Schreib Juri <juri@schreib.at>
2021-02-24 22:54:04 +0100

    Add check if env var is set

--- siteMetaIndexer.sh
+++ siteMetaIndexer.sh
@@ -3,11 +3,15 @@
 index=$1
 count=$2
 
-./esClient/searchIndex.sh $index $count |
-./esClient/extractLinks.sh | 
-parallel --colsep ' ' --linebuffer ./updateLinkContentType.sh $index |
-./esClient/buildBulkUpdateDocument.sh $index |
-./esClient/updateDocsBulk.sh && echo
+[ -n "$SIDX_ES_HOST" ] &&
+    ./esClient/searchIndex.sh $index $count |
+    ./esClient/extractLinks.sh | 
+    parallel --no-run-if-empty --colsep ' ' --linebuffer ./updateLinkContentType.sh $index |
+    ./esClient/buildBulkUpdateDocument.sh $index |
+    ./esClient/updateDocsBulk.sh && echo ||
+
+    echo 'Please set the environment Variable $SIDX_ES_HOST to the elasticsearch to continue' && 
+    exit 1
 
 
 
