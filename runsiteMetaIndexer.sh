3051ed60afe9823564c346db7fba2d93aa576b6f
Juri Schreib <schreibj@esserver01.localdomain>
2021-02-25 21:18:18 +0100

    Add break condition if no more results can be crawled

--- runsiteMetaIndexer.sh
+++ runsiteMetaIndexer.sh
@@ -5,6 +5,9 @@ export SIDX_ES_HOST="127.0.0.1:9200"
 TMPFILE="/tmp/siteIndexer_$1"
 
 while ./siteMetaIndexer.sh $1 1000 > $TMPFILE; do
+
+[ -s $TMPFILE ] || break
+
 < $TMPFILE
 sed 's/\[\[nl\]\]/\n/g' $TMPFILE |
 ./esClient/updateDocsBulk.sh && echo
